7665a90d1dfa8d1687e5f3350a8b2da1cef34fd6
Iryna Walsh <i2pi8bl7@students.codeinstitute.net>
2024-10-30 00:40:20 +0000

    Create:primary menu function and implement and test the visual libraries

--- run.py
+++ run.py
@@ -2,10 +2,10 @@ import gspread
 import datetime
 from google.oauth2.service_account import Credentials
 from colorama import init, Fore, Style
-from pyfiglet import Figlet
+import pyfiglet
 
 # Initialize colorama library
-init()
+init(autoreset=True)
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -18,7 +18,22 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("adhd_medication_tracker")
 
-ritalin = SHEET.worksheet("Ritalin")
+def primary_menu():
+    """
+    Display the primary menu where the user is introduced to the tracker and
+    can input one of the options:
+    1. Track current medication
+    2. Create new medication tracking
+    3. Exit
+    """
+    header_ascii = pyfiglet.figlet_format("Welcome to ADHD Medication Tracker", font="smslant")
+    print(Fore.YELLOW + header_ascii)
+    print(Fore.CYAN + "Please choose an option:\n")
+    print("1. Track current medication")
+    print("2. Create new medication tracking")
+    print("3. Exit\n")
 
-data = ritalin.get_all_values()
-print(data)
+    choice = input(Fore.CYAN + "Make your choice (1/2/3): ")
+    print(Fore.GREEN + f"Your choice is {choice}")
+
+primary_menu()    
