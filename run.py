88f0cf4ecd7612458b217f91eefd8a58d7f1ad83
Iryna Walsh <i2pi8bl7@students.codeinstitute.net>
2024-11-07 00:08:18 +0000

    Working on new_log function logic and create improved validate_date function

--- run.py
+++ run.py
@@ -27,7 +27,7 @@ def add_medication(SHEET):
     Create a new worksheet with a new medication name and headings to log progress
     """
     try:
-        medication_name = input(Fore.LIGHTBLACK_EX + "Enter the new medication name: \n" + Fore.RESET)
+        medication_name = input(Fore.LIGHTBLACK_EX + "Enter the new medication name: \n" + Fore.RESET).capitalize()
         new_medication = SHEET.add_worksheet(title=medication_name, rows='100', cols='9')
         new_medication.append_row(
             ["Date", "Dose(mg)", "Intake per day", "First dose intake", "Second dose intake", "Third dose intake", "Efficacy(1-10)", "Side effects", "Personal observations"]
@@ -36,21 +36,33 @@ def add_medication(SHEET):
         return new_medication
     except Exception as e:
         print(f"Could not create a worksheet: {e}")
-        
-#def validate_date():
+
+def validate_date(worksheet):
+    """
+    Logic that handles current date autofill 
+    and skips it if the current date log was already filled
+    """
+    date_log = datetime.today().date().strftime("%d/%m/%Y")
+    if worksheet.findall(date_log):
+        print("Today's log already exists, skipping...")
+        return False
+    else:
+        return True
+
+#def past_date():
 #    """
-#    Logic to validate current date input and return error if the wrong format or old date is applied
+#    Logic to validate past date input and return error if the wrong format or future date is applied
 #    """
 #    while True:
-#        date_choice = input("Create a log for today's date? Y/N\n")
-#        if 'y' in date_choice.lower():
+#        date_choice = input("Create a log for a missed log date?: Y/N \n")
+#        if 'n' in date_choice.lower():
 #            print("Creating a log for today")
-#            return datetime.today().date()
+#            validate_date(worksheet)
 #        else:
-#            date_entry = input(Fore.LIGHTBLACK_EX +"Enter date in this format DD/MM/YYY: \n" + Fore.RESET)
+#            date_entry = input(Fore.LIGHTBLACK_EX +"Enter missed date in this format DD/MM/YYY: \n" + Fore.RESET)
 #            today_date = datetime.today().date()
 #            try:
-#                date_entry = datetime.strptime(date_entry, "%d/%m/%Y") # convert from string to datetime object
+#                date_entry = datetime.strptime(date_entry, "%d/%m/%Y") 
 #                if date_entry.date() <= today_date:
 #                    print(f"{date_entry} was successfully logged")
 #                else:
@@ -68,10 +80,20 @@ def new_log(SHEET):
     
     try:
         medication_worksheet = SHEET.worksheet(choose_medication)
+        if not validate_date(medication_worksheet):
+            return
         # Add current date autofill
         print("Creating a log for today")
-        date_log = datetime.today().date().strftime("%d/%m/%Y")
-        medication_worksheet.append_row(date_log)
+        # get input arguments from user ...
+        dose = input("Dose (mg): ")
+        intake_per_day = input("Intake per day: ")
+        first_dose = input("Have you taken your first dose today: Y/N")
+        second_dose = input("Have you taken your second dose today: Y/N")
+        third_dose = input("Have you taken your third dose today: Y/N")
+        efficacy = input("How effective did you find the medication?: (1-10)")
+        side_effects = input("Have you experienced any side effects today?: Y/N")
+        personal_observation = input("Describe in short, personal observations regarding your experience: max 30 words")
+        medication_worksheet.append_row([date_log])
     except gspread.exceptions.WorksheetNotFound:
         print(Fore.RED + f"Medication with a name'{choose_medication}'does not exist")
         add_medication(SHEET) # If medication does not exist you can opt to create new one
